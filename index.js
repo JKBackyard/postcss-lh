3f696c027b90ed14b96ade71dfc10a631802845f
James Kolce <contact@jameskolce.com>
2016-06-18 20:31:46 -0600

    [fix: #5] Use replaceValues to simplify code

--- index.js
+++ index.js
@@ -8,30 +8,12 @@ const defaults = {
 };
 
 module.exports = postcss.plugin('postcss-lh', (opts = defaults) => {
-  let lineHeight;
-
   return (css) => {
-    css.walkDecls((decl) => {
+    const lineHeight = getLineHeight(css, opts);
+    const lhReg = new RegExp('\\d*\\.?\\d+' + opts.rhythmUnit, 'gi');
 
-      // Check if the declaration is not inside the a print media query
-      // if is the root selector and if is a font property
-      if (decl.parent.parent.params !== 'print' &&
-          decl.parent.selector === opts.rootSelector &&
-          (decl.prop === 'font' || decl.prop === 'line-height')) {
-
-        // Get the line-height
-        lineHeight = getLineHeight(decl);
-      }
-
-      // Check if this declaration has a rhythm unit on it
-      if (~ decl.value.indexOf(opts.rhythmUnit)) {
-
-        // RegExp to capture declaration value
-        const regexp = new RegExp('\\d*\\.?\\d+' + opts.rhythmUnit, 'gi');
-
-        // Replace the current declaration value with the transformed value
-        decl.value = decl.value.replace(regexp, (val) => lhToRem(parseFloat(val), lineHeight));
-      }
+    css.replaceValues(lhReg, {fast: opts.rhythmUnit}, (val) => {
+      return lhToRem(parseFloat(val), lineHeight);
     });
   };
 });
